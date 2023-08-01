7eb316ea689c18183aeb114ed5ff049f9d2e969f
zeponcantos <czepon@gmail.com>
2023-07-31 19:16:13 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,16 +7,22 @@ app.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: '/images/in-n-out-burger.jpeg'
+        pic: '/images/in-n-out-burger.jpg' 
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: '/images/mcdonalds-sign.jpeg'
+        pic: '/images/mcdonalds-sign.jpg'
+        
       }]
+// router.get('/new', (req, res) => {
+//         res.render('places/new')
+//       })
+      
       
       res.render('places/index', { places })
+      
   })
   
 
