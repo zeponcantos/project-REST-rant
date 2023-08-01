6fefa58247ae3ac75ded9b17fe47a2f5cb2c2161
zeponcantos <czepon@gmail.com>
2023-07-31 21:41:05 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 // GET /places
 router.get('/', (req, res) => {
       
@@ -24,10 +24,23 @@ router.get('/', (req, res) => {
         res.render('places/new')
   });
 
-        router.post('/', (req, res) => {
-        console.log(req.body)
-        res.send('POST /places')
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
+  
   
 
 module.exports = router
