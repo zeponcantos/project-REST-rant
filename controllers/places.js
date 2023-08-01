c13a5402da32d3b0523dc5205ac2abd3ddbeae68
zeponcantos <czepon@gmail.com>
2023-07-31 19:46:24 -0700

    test

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,7 @@ const app = require('express').Router()
 
 // GET /places
 app.get('/', (req, res) => {
+  
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -14,30 +15,14 @@ app.get('/', (req, res) => {
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: '/images/mcdonalds-sign.jpg'
-        
-      }]
-// router.get('/new', (req, res) => {
-//         res.render('places/new')
-//       })
-      
-      
+      }];
       res.render('places/index', { places })
       
+      
   })
-  
+  //       router.get('/new', (req, res) => {
+  //       res.render('places/new')
+  // });
 
 module.exports = app
 
-// let places = [{
-//   name: 'H-Thai-ML',
-//   city: 'Seattle',
-//   state: 'WA',
-//   cuisines: 'Thai, Pan-Asian',
-//   pic: '/images/in-n-out-burger.jpeg'
-// }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/mcdonalds-sign.jpeg'
-// }];
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,14 +1,14 @@
-// const React = require('react')
-// const Def = require('../default')
+const React = require('react')
+const Def = require('../default')
 
-// function new_form () {
-//     return (
-//         <Def>
-//           <main>
-//             <h1>Add a New Place</h1>
-//           </main>
-//         </Def>
-//     )
-// }
+function new_form () {
+    return (
+        <Def>
+          <main>
+            <h1>Add a New Place</h1>
+          </main>
+        </Def>
+    )
+}
 
-// module.exports = new_form
+module.exports = new_form
